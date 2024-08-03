9181f89898f983a4324c95866b8d7d6a0cd172ff
olliejohnson <oliver.joseph.johnson@gmail.com>
2024-08-03 13:29:23 -0400

    Added stuff

--- custom_components/secure_access/__init__.py
+++ custom_components/secure_access/__init__.py
@@ -1,7 +1,22 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 
 from .const import DOMAIN
 
-async def async_setup(hass: HomeAssistant, config: ConfigEntry) -> bool:
+from . import access_server
+
+PLATFORMS = [Platform.COVER]
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = access_server.AccessServer(hass, entry.data["ip"])
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    
+    return unload_ok
--- custom_components/secure_access/config_flow.py
+++ custom_components/secure_access/config_flow.py
@@ -1,23 +1,68 @@
 from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
+from homeassistant import exceptions
 from .const import DOMAIN
+from .access_server import AccessServer
+
+from typing import Any
+import logging
 
 import voluptuous as vol
 
+_LOGGER = logging.getLogger(__name__)
+
 DATA_SCHEMA = vol.Schema({vol.Required("ip"): str, vol.Optional("checkbox"): bool})
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    async def async_step_user(self, info):
-        if info is not None:
-            pass
-
-        return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA
-        )
+async def validate_input(hass: HomeAssistant, data: dict) -> dict[str, Any]:
+    if len(data["ip"]) < 7:
+        raise InvalidIp
     
-    async def async_set_unignore(self, user_input):
-        unique_id = user_input["unique_id"]
-        await self.async_set_unique_id(unique_id)
+    if data["checkbox"] == False:
+        raise NoTOS
+    
+    access_server = AccessServer(hass, data["ip"])
+
+    result = await access_server.test_connection()
+    if not result:
+        raise ASConnectFailed
+    
+    return {"title": data["host"]}
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
+    VERSION = 1
+    MINOR_VERSION = 0
+
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
+
+    async def async_step_user(self, user_input=None):
+
+        errors = {}
+
+        if user_input is not None:
+            try:
+                info = await validate_input(self.hass, user_input)
+
+                return self.async_create_entry(title=info["title"], data=user_input)
+            except ASConnectFailed:
+                errors["base"] = "connection_failed"
+            except InvalidIP:
+                errors["ip"] = "invalid_ip"
+            except NoTOS:
+                errors["checkbox"] = "no_agreement"
+            except Exception:
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
 
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
+
+class ASConnectFailed(exceptions.HomeAssistantError):
+    """"""
+
+class InvalidIp(exceptions.HomeAssistantError):
+    """"""
+
+class NoTOS(exceptions.HomeAssistantError):
+    """"""
